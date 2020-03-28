2fb4ce237b664ee2d3fca9fe470d2ed6418dda44
djy <137359049@qq.com>
2020-03-28 09:26:01 +0800

    1.编译报错处理

--- app/src/main/java/me/jessyan/rxerrorhandler/demo/MainActivity.kt
+++ app/src/main/java/me/jessyan/rxerrorhandler/demo/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017 JessYan
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package me.jessyan.rxerrorhandler.demo
 
 import android.os.Bundle
@@ -43,8 +28,8 @@ class MainActivity : AppCompatActivity() {
                 .retryWhen(RetryWithDelay(3, 2)) //retry(http connect timeout)
                 .subscribe(object : ErrorHandleSubscriber<Any>(rxErrorHandler) {
                     override fun onNext(o: Any) {}
-                    override fun onError(t: Throwable?) {
-                        Log.e("xia", "t1:" + t?.message)
+                    override fun onError(t: Throwable) {
+                        Log.e("xia", "t1:" + t.message)
                     }
                 })
 
